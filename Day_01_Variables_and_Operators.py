c6e2ac1b8239694cc2adb49d37d9d8b85f9fa925
Ayesha Afzal <shehzadiayesha998@gmail.com>
2026-03-29 10:39:36 +0500

    Add Day 1 practice: Variables and Arithmetic
    Includes basic input/output and math operators practice.

---
+++ Day_01_Variables_and_Operators.py
@@ -0,0 +1,43 @@
+#variables
+name = "Sara"          
+age = 22               
+height = 5.4           
+is_student = True     
+print("Assalam-o-Alaikum!")
+print("Name:", name)
+print("Age:", age)
+
+
+user_city = input("Enter your city name:")
+print("Ma Sha Allah, " + user_city + " is very beautiful city!")
+
+#addition
+a = 2
+b = 4
+result = a + b
+print("Result of addition is:", result, "\n")
+print("***************************")
+print()                  
+
+#subtraction
+a = 5
+b = 3
+result = a - b
+print("Result of subtraction is:", result, "\n")
+print("***************************")
+print()
+
+#multiplication
+a = 5
+b = 6
+result = 5 * 6
+print("Result of multiplication is:", result, "\n")
+print("***************************")
+print()
+
+#division
+a = 25
+b = 4
+result = a / b
+print("Result of division is:", result, "\n")
+print("***************************")
